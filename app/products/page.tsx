767db7092b52792f5a839c56b0894e72fafbbd7e
antonythomas123 <antonythomas993@gmail.com>
2025-09-03 17:04:21 +0530

    Ecommerce site

---
+++ app/products/page.tsx
@@ -0,0 +1,16 @@
+import { ProductList } from "@/components/porduct-list";
+import { stripe } from "@/lib/stripe";
+
+export default async function Products() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  return (
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All products
+      </h1>
+      <ProductList products={products.data} />
+    </div>
+  );
+}
